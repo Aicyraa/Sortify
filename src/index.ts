08f65ce51b829ceb72cb77a9f50369112f2e0b8e
jee <jeecoding09@gmail.com>
2026-06-13 10:52:26 +0800

    IIFE for main function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-h
+
+
+// get folder or files
+// process it
+const path = require("node:path");
+
+((args: string[]): void => {
+  // process args, get the folder, 
+  // get absolute file path
+  const [folder, ...command] = args
+  const targetPath: string = path.resolve(folder)
+  console.log(targetPath)
+  
+})(process.argv.slice(2));
+
+function Sort(): void {
+  // get folder or files
+  // sort it
+}
+
+function DryRun(): void {
+  //, get folder and file
+}
+
+function Undo(): void {
+  // check logger.json
+  // get folder or files
+  // process it
+}
