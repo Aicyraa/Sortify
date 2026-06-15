c4434946a526d26afb1609aae841dab7ce3f1c46
jee <jeecoding09@gmail.com>
2026-06-15 11:14:22 +0800

    feat: DryRun Flag

---
+++ src/dryRun.ts
@@ -0,0 +1,17 @@
+import * as fs from "node:fs";
+import { processEntries , scanFolder } from "./helper.ts"
+
+export default function dryRun(folderPath: string): void {
+  
+  if (!fs.existsSync(folderPath)) {
+    console.log("FFolder does not exist!")
+    process.exit(1)
+  }
+  
+  const { fileCount, processed } = scanFolder(folderPath);
+  const fileObjects = processEntries(processed);
+  
+  console.log("DryRun success", fileCount)
+  
+  
+}
